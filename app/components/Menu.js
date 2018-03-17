6453ce707b7078e6557ec814ee072ba80a24044b
SokichiFujita <sokichi.fujita@gmail.com>
2018-03-17 18:03:36 +0900

    initial commit

---
+++ app/components/Menu.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
+import { Link } from 'react-router-dom';
+import ListSubheader from 'material-ui/List/ListSubheader';
+import List, { ListItem } from 'material-ui/List';
+import Drawer from 'material-ui/Drawer';
+
+class Menu extends Component {
+  render() {
+    return (
+      <Drawer variant="permanent" style={this.props.style}>
+        <List style={this.props.style}>
+          <ListSubheader>Menu</ListSubheader>
+          <ListItem component={Link} to="/">Top</ListItem>
+          <ListItem component={Link} to="/sample">Sample</ListItem>
+        </List>
+      </Drawer>
+    );
+  }
+}
+
+Menu.propTypes = {
+  width: PropTypes.number
+}
+
+export default Menu;
---
+++ app/components/Navi.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'; 
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+class Navi extends Component {
+  render() {
+    return (
+      <AppBar position="absolute" style={this.props.style}>
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            {this.props.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+Navi.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Navi;
